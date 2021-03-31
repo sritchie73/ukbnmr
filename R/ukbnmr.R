c183f9da068e578af9be59873e3b61dc0c56a6d7
Scott Ritchie <sritchie73@gmail.com>
2021-03-31 18:24:26 +0100

    Updated package documentation

--- R/ukbnmr.R
+++ R/ukbnmr.R
@@ -7,12 +7,22 @@
 #' output by \href{https://biobank.ctsu.ox.ac.uk/crystal/exinfo.cgi?src=accessing_data_guide}{ukbconv},
 #' extract the NMR biomarker fields and give them short comprehensible column
 #' names. Measurements are also split into multiple rows where a participant has
-#' measurements at both baseline and first repeat assessment.
+#' measurements at both baseline and first repeat assessment. All functions below
+#' will also work with raw data extracted from UK Biobank.
 #'
 #' The \code{\link{compute_nightingale_ratios}()} function will compute the
 #' \href{https://nightingalehealth.com/biomarkers}{81 Nightingale Health biomarker ratios}
-#' from the 168 biomarkers available for download from
+#' that were missing prior to the May update of
 #' \href{https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=220}{UK Biobank}.
+#'
+#' The \code{\link{compute_extended_ratios}()} function will compute an extended
+#' set of biomarker ratios expanding on the biomarkers available directly from
+#' the Nightingale platform.
+#'
+#' The \code{\link{recompute_derived_biomarkers}()} function will recompute all
+#' composite biomarkers and ratios from 107 non-derived biomarkers, which is
+#' useful for ensuring data consistency when adjusting for unwanted biological
+#' variation.
 #'
 #' This package also provides a \code{data.frame} of biomarker information, loaded
 #' as \code{\link{nmr_info}}.
